281782ebe44ef9e556d45cae6a80d5acbf5ccc8b
coolkid <quang0984006938@gmail.com>
2026-04-27 19:14:48 +0700

    consume data from kafka

--- src/main/java/FlinkCommerce/DataStreamJob.java
+++ src/main/java/FlinkCommerce/DataStreamJob.java
@@ -8,6 +8,16 @@ public class DataStreamJob {
 
         String topic = "financial-transactions";
 
+        KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics(topic)
+                .setGroupId("flink-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new JsonDeserializationSchema())
+                .build();
+
+        DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka source");
+
         env.execute("Flink API Skeleton");
     }
 }
